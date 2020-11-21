9186fa8d19bb5fb43bbcd19209c92e27b9832e24
nanteqiao <1240145882@qq.com>
2020-11-21 19:00:49 +0800

    网站头部 未完成

--- js/index.js
+++ js/index.js
@@ -1,22 +1,49 @@
-<script>        
-  var mySwiper = new Swiper ('.swiper-container', {
-    direction: 'vertical', // 垂直切换选项
-    loop: true, // 循环模式选项
-    
-    // 如果需要分页器
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    
-    // 如果需要前进后退按钮
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    
-    // 如果需要滚动条
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })        
-</script>
+// $('.menuDown').on('mouseover',function(){
+// 	$('.dropdown').addClass('animated bounceInDown');
+// })
+// $('.menuDown').on('mouseout',function(){
+// 	$('.dropdown').addClass('animated bounceOutUp ');
+// })
+
+// $('.menuDown').on('mouseover',function(){
+// 	$('.dropdown').animate({
+// 		opacity:1,
+// 		top:26
+// 	},300)
+// })
+// $('.menuDown').on('mouseout',function(){
+// 	$('.dropdown').animate({
+// 		opacity:0,
+// 		top:20
+// 	},300);
+// })
+var mySwiper = new Swiper ('.swiper-container', {
+			    direction: 'horizontal', // 垂直切换选项
+			    loop: true, // 循环模式选项
+			    autoplay: {
+			       delay: 3000,
+			       stopOnLastSlide: false,
+			       disableOnInteraction: false,
+			       },
+			       effect : 'flip',
+			       // slidesPerView: 3,
+			       // centeredSlides: true,
+			    // 如果需要分页器
+			    pagination: {
+			      el: '.swiper-pagination',
+			    },
+			    
+			    // 如果需要前进后退按钮
+			    navigation: {
+			      nextEl: '.swiper-button-next',
+			      prevEl: '.swiper-button-prev',
+			    },
+			    
+			    // 如果需要滚动条
+			    scrollbar: {
+			      el: '.swiper-scrollbar',
+			    },
+	})   
+	 
+	
+	
