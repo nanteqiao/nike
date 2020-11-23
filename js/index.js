d66f3d348c12ff1cef37a97092e0480f7b50713a
nanteqiao <1240145882@qq.com>
2020-11-23 23:02:41 +0800

    首页商品列表

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-// $('.menuDown').on('mouseover',function(){
-// 	$('.dropdown').addClass('animated bounceInDown');
-// })
-// $('.menuDown').on('mouseout',function(){
-// 	$('.dropdown').addClass('animated bounceOutUp ');
-// })
-
-// $('.menuDown').on('mouseover',function(){
-// 	$('.dropdown').animate({
-// 		opacity:1,
-// 		top:26
-// 	},300)
-// })
-// $('.menuDown').on('mouseout',function(){
-// 	$('.dropdown').animate({
-// 		opacity:0,
-// 		top:20
-// 	},300);
-// })
 var mySwiper = new Swiper ('.swiper-container', {
 		direction: 'horizontal', // 垂直切换选项
 		loop: true, // 循环模式选项
@@ -45,5 +26,80 @@ var mySwiper = new Swiper ('.swiper-container', {
 		},
 	})   
 	 
+	// $(document).scrollTop();
+ window.addEventListener("scroll",()=>{  
+     let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;    
+     let offsetTop = document.querySelector('.wall-header').offsetTop;
+	 
+     if (offsetTop - scrollTop < 100) {  
+		  document.querySelector('.wall-header').style.position="fixed";
+          document.querySelector('.wall-header').style.top="90";
+     } else {
+	 	   document.querySelector('.wall-header').style.position="relative";  
+	       document.querySelector('.wall-header').style.top="120";  
+	  }
+	 
+	 }) 
+	  
+	  
+$(function (){
+
+  // 获取商品列表数据
+  $.ajax({
+    url: '../productType.json',
+    type: 'get',
+    dataType: 'json',
+    success: function (json){
+      // console.log(json)
+      var goodsStr = ''
+      $.each(json,function (index,item){
+        goodsStr += `<li><a>${item}</a></li>`
+      })
+      $('.product').html(goodsStr)
+    }
+  })
+  
+  $.ajax({
+    url: '../goods.json',
+    type: 'get',
+    dataType: 'json',
+    success: function (json){
+      // console.log(json)
+      var goodsStr = ''
+      $.each(json,function (index,item){
+        goodsStr += `<div class="details"><img src="${item.imgurl}" >
+			<div class="product-card">
+				<div class="product-card_info">
+					<div class="product-card_info_new">
+						${item.new}
+					</div>
+					<div class="product-card_info_titles">
+						<div class="product-card_info_title">
+							${item.title}
+						</div>
+						<div class="product-card__subtitle">
+							${item.type}
+						</div>
+					</div>
+				</div>
+				<div class="product-card_color">
+					${item.color}
+				</div>
+				<div class="product-card_price">
+					${item.price}
+				</div>
+			</div></div>`
+      })
+      $('.main_center').html(goodsStr)
+    }
+  })
+})
+	  
+
+$('.menu').on('mouseenter','li',function(){
+	$('li').parent().parent().height(500);
 	
-	
+})
+$('.menu').on('mouseout','li',function(){
+	$('li').parent().parent().height(30)
+})
