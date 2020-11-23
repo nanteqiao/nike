a14a538aa74ad4f096c82f79b60d914cc3662cc1
nanteqiao <1240145882@qq.com>
2020-11-23 10:59:22 +0800

    头部更新

--- js/index.js
+++ js/index.js
@@ -18,31 +18,31 @@
 // 	},300);
 // })
 var mySwiper = new Swiper ('.swiper-container', {
-			    direction: 'horizontal', // 垂直切换选项
-			    loop: true, // 循环模式选项
-			    autoplay: {
-			       delay: 3000,
-			       stopOnLastSlide: false,
-			       disableOnInteraction: false,
-			       },
-			       effect : 'flip',
-			       // slidesPerView: 3,
-			       // centeredSlides: true,
-			    // 如果需要分页器
-			    pagination: {
-			      el: '.swiper-pagination',
-			    },
-			    
-			    // 如果需要前进后退按钮
-			    navigation: {
-			      nextEl: '.swiper-button-next',
-			      prevEl: '.swiper-button-prev',
-			    },
-			    
-			    // 如果需要滚动条
-			    scrollbar: {
-			      el: '.swiper-scrollbar',
-			    },
+		direction: 'horizontal', // 垂直切换选项
+		loop: true, // 循环模式选项
+		autoplay: {
+		   delay: 3000,
+		   stopOnLastSlide: false,
+		   disableOnInteraction: false,
+		   },
+		   effect : 'slide',
+		   // slidesPerView: 3,
+		   // centeredSlides: true,
+		// 如果需要分页器
+		pagination: {
+		  el: '.swiper-pagination',
+		},
+		
+		// 如果需要前进后退按钮
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+		
+		// 如果需要滚动条
+		scrollbar: {
+		  el: '.swiper-scrollbar',
+		},
 	})   
 	 
 	
